685832310ad3bd3b7ca063bdfafd851a317e5a74
Yash <jainy092005@gmail.com>
2024-07-31 20:56:41 +0530

    suggested users fetched

--- client/src/components/common/RightPanel.jsx
+++ client/src/components/common/RightPanel.jsx
@@ -1,9 +1,29 @@
 import { Link } from "react-router-dom";
 import RightPanelSkeleton from "../skeletons/RightPanelSkeleton";
-import { USERS_FOR_RIGHT_PANEL } from "../../utils/db/dummy.js";
+// import { USERS_FOR_RIGHT_PANEL } from "../../utils/db/dummy.js";
+
+import { useQuery } from "react-query";
 
 const RightPanel = () => {
-  const isLoading = false;
+  const { data: suggestedUsers, isLoading } = useQuery({
+    queryKey: ["suggestedUsers"],
+    queryFn: async () => {
+      try {
+        const res = await fetch(`/api/users/suggested`);
+
+        const data = await res.json();
+        if (!res.ok)
+          throw new Error(data.error || "Failed to fetch suggested users");
+
+        console.log(data);
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+  });
+
+  if (suggestedUsers?.length === 0) return <div className="md:w-64 w-0"></div>;
 
   return (
     <div className="hidden lg:block my-4 mx-2">
@@ -20,7 +40,7 @@ const RightPanel = () => {
             </>
           )}
           {!isLoading &&
-            USERS_FOR_RIGHT_PANEL?.map((user) => (
+            suggestedUsers?.map((user) => (
               <Link
                 to={`/profile/${user.username}`}
                 className="flex items-center justify-between gap-4"
@@ -29,7 +49,9 @@ const RightPanel = () => {
                 <div className="flex gap-2 items-center">
                   <div className="avatar">
                     <div className="w-8 rounded-full">
-                      <img src={user.profileImg || "/avatar-placeholder.png"} />
+                      <img
+                        src={user.profileImage || "/avatar-placeholder.png"}
+                      />
                     </div>
                   </div>
                   <div className="flex flex-col">
@@ -37,7 +59,7 @@ const RightPanel = () => {
                       {user.fullName}
                     </span>
                     <span className="text-sm text-slate-500">
-                      @{user.username}
+                      @{user.userName}
                     </span>
                   </div>
                 </div>
